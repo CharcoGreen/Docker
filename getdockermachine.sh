9b5fe4360b9881459fbab9929d5ce157ae7fc417
CharcoGreen <dani.flaco0@gmail.com>
2018-06-01 09:17:25 +0200

    Create getdockermachine.sh

---
+++ getdockermachine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script para descargar e instalar docker machine en linux
+
+base=https://github.com/docker/machine/releases/download/v0.14.0 &&
+curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+sudo install /tmp/docker-machine /usr/local/bin/docker-machine
