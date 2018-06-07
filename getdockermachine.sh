fe42ad29201811cb05215dbcb90534e7afb344e8
CharcoGreen <dani.flaco0@gmail.com>
2018-06-07 12:53:18 +0200

    Update getdockermachine.sh

--- getdockermachine.sh
+++ getdockermachine.sh
@@ -2,6 +2,14 @@
 
 # Script para descargar e instalar docker machine en linux
 
+# Instala docker-machine
 base=https://github.com/docker/machine/releases/download/v0.14.0 &&
 curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
 sudo install /tmp/docker-machine /usr/local/bin/docker-machine
+
+# Instala herramientas básicas
+base=https://raw.githubusercontent.com/docker/machine/v0.14.0
+for i in docker-machine-prompt.bash docker-machine-wrapper.bash docker-machine.bash
+do
+wget "$base/contrib/completion/bash/${i}" -P /etc/bash_completion.d
+done
